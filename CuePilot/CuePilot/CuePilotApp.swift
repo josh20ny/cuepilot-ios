e9bba20db56933c3bfffc331a07483d79356acbf
Chris McSorley <chrismc@austin.rr.com>
2026-02-22 17:05:44 -0600

    Initial Project

---
+++ CuePilot/CuePilot/CuePilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  CuePilotApp.swift
+//  CuePilot
+//
+//  Created by Chris McSorley on 2/22/26.
+//
+
+import SwiftUI
+
+@main
+struct CuePilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CuePilot/CuePilotTests/CuePilotTests.swift
@@ -0,0 +1,17 @@
+//
+//  CuePilotTests.swift
+//  CuePilotTests
+//
+//  Created by Chris McSorley on 2/22/26.
+//
+
+import Testing
+@testable import CuePilot
+
+struct CuePilotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
